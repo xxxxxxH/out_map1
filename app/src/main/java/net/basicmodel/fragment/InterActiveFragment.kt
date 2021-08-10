6f59a618e8dafb4801ed1a37a329add3f30599fc
v_xhangxie <v_xhangxie@tencent.com>
2021-08-10 18:36:12 +0800

    2021年8月10日 18:36:00

---
+++ app/src/main/java/net/basicmodel/fragment/InterActiveFragment.kt
@@ -0,0 +1,13 @@
+package net.basicmodel.fragment
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Copyright (C) 2021,2021/8/10, a Tencent company. All rights reserved.
+ *
+ * User : v_xhangxie
+ *
+ * Desc :
+ */
+class InterActiveFragment : Fragment() {
+}
